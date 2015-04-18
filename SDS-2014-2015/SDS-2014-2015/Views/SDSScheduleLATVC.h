689f592af71d2d617d50f825e3fa62dbdcc3efcf
blakecrosley <blake.crosley@gmail.com>
2015-04-18 14:59:01 -0700

    Update the schedule for LA and remove NYC.

---
+++ SDS-2014-2015/SDS-2014-2015/Views/SDSScheduleLATVC.h
@@ -0,0 +1,13 @@
+//
+//  SDSScheduleLATVC.h
+//  SDS-2014-2015
+//
+//  Created by Blake Crosley on 4/18/15.
+//  Copyright (c) 2015 SoulDeSoul, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDSScheduleLATVC : UITableViewController
+
+@end
