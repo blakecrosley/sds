550ae8bb3c779712a3b8ee15980dfd19aded6220
blakecrosley <blake.crosley@gmail.com>
2015-12-01 21:59:51 -0800

    Version 2.0.1.

---
+++ SDS-2014-2015/SDS-2014-2015/Views/SDSRochesterTVC.h
@@ -0,0 +1,13 @@
+//
+//  SDSRochesterTVC.h
+//  Soul de Soul
+//
+//  Created by Blake Crosley on 11/3/15.
+//  Copyright © 2015 Soul de Soul, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDSRochesterTVC : UITableViewController
+
+@end
